a22a4027b676d9fabce4766f8ccd35253604536b
sor4chi <sorakawa0218@gmail.com>
2023-08-13 00:53:24 +0900

    feat: bind remix service to gateway

--- workers/gateway/src/index.ts
+++ workers/gateway/src/index.ts
@@ -2,12 +2,17 @@ import { Hono } from 'hono'
 
 type Bindings = {
   PRIVATE_SERVICE: Fetcher;
+  WEBSITE_SERVICE: Fetcher;
   INTERNAL_TOKEN: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>()
 
-app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/', async (c) => {
+  const res = await c.env.WEBSITE_SERVICE.fetch(c.req.raw)
+  return res
+})
+
 app.get('/private/*', async (c) => {
   const res = await c.env.PRIVATE_SERVICE.fetch(c.req.raw, {
     headers: {
